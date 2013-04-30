2b4f32cc2ff61a03635ba7e189e9fb0403a50db2
Thomas Refis <refis.thomas@gmail.com>
2013-04-30 16:41:19 +0200

    First stable version.

---
+++ autoload/coquille.py
@@ -0,0 +1,488 @@
+import vim
+
+import re
+import subprocess
+import xml.etree.ElementTree as ET
+
+from collections import deque
+
+import vimbufsync
+vimbufsync.check_version("0.1.0", who="coquille")
+
+#: Pipe used to discuss with coqtop
+coqtop = None
+
+#: See vimbufsync ( https://github.com/def-lkb/vimbufsync )
+saved_sync = None
+
+#: Keeps track of what have been checked by Coq, and what is waiting to be
+#: checked.
+encountered_dots = []
+send_queue = deque([])
+
+error_at = None
+
+info_msg = None
+
+###################
+# synchronization #
+###################
+
+def sync():
+    global saved_sync
+    curr_sync = vimbufsync.sync()
+    if not saved_sync or curr_sync.buf() != saved_sync.buf():
+        _reset()
+    else:
+        (line, col) = saved_sync.pos()
+        rewind_to(line - 1, col) # vim indexes from lines 1, coquille from 0
+    saved_sync = curr_sync
+
+def _reset():
+    global saved_sync, encountered_dots, info_msg, error_at, send_queue
+    encountered_dots = []
+    send_queue = deque([])
+    saved_sync = None
+    error_at   = None
+    info_msg   = None
+    reset_color()
+
+#####################
+# exported commands #
+#####################
+
+def kill_coqtop():
+    global coqtop
+    if coqtop:
+        try:
+            coqtop.terminate()
+            coqtop.communicate()
+        except OSError:
+            pass
+        coqtop = None
+    _reset()
+
+def restart_coq():
+    global coqtop
+    if coqtop: kill_coqtop()
+    try:
+        coqtop = subprocess.Popen(
+                ["coqtop", "-ideslave"],
+                stdin = subprocess.PIPE,
+                stdout = subprocess.PIPE
+                )
+    except OSError:
+        print("Error: couldn't launch coqtop")
+
+def coq_rewind(steps=1):
+    if coqtop is None:
+        print("Error: Coqtop isn't running. Are you sure you called :CoqLaunch?")
+        return
+
+    global encountered_dots
+
+    request = ET.Element('call')
+    request.set('val', 'rewind')
+    request.set('steps', str(steps))
+
+    send_cmd(request)
+
+    response = get_answer()
+
+    if response.get('val') == 'good':
+        encountered_dots = encountered_dots[:len(encountered_dots) - steps]
+
+    refresh()
+
+def coq_to_cursor():
+    if coqtop is None:
+        print("Error: Coqtop isn't running. Are you sure you called :CoqLaunch?")
+        return
+
+    sync()
+
+    cursor_pos = vim.current.window.cursor
+    (line, col, _)  = encountered_dots[-1] if encountered_dots else (0,0, False)
+
+    (cline, ccol) = cursor_pos
+
+    while True:
+        r = _get_message_range((line, col))
+        if r is not None and r['stop'] < (cline - 1, ccol):
+            line = r['stop'][0]
+            col  = r['stop'][1] + 1
+            send_queue.append(r)
+        else:
+            break
+
+    send_until_fail()
+
+def coq_next():
+    if coqtop is None:
+        print("Error: Coqtop isn't running. Are you sure you called :CoqLaunch?")
+        return
+
+    sync()
+
+    (line, col, _)  = encountered_dots[-1] if encountered_dots else (0,0, False)
+    message_range = _get_message_range((line, col))
+    send_queue.append(message_range)
+
+    send_until_fail()
+
+def launch_coq():
+    restart_coq()
+
+def debug():
+    if encountered_dots:
+        print("encountered dots = [")
+        for (line, col, collapsable) in encountered_dots:
+            print("  (%d, %d, %s) ; " % (line, col, collapsable))
+        print("]")
+
+#####################################
+# IDE tools: Goal, Infos and colors #
+#####################################
+
+def refresh():
+    show_goal()
+    show_info()
+    reset_color()
+
+def show_goal():
+    cmd = ET.Element('call')
+    cmd.set('val', 'goal')
+
+    send_cmd(cmd)
+
+    buff = None
+    for b in vim.buffers:
+        if re.match(".*Goals$", b.name):
+            buff = b
+            break
+    del buff[:]
+
+    shitty_xml = get_answer()
+    opt_goal = shitty_xml.find('option')
+    if opt_goal is None or opt_goal.get('val') == 'none': return # nothing to do
+
+    sub_goals = list(opt_goal.find('goals').find('list')) # there are only 2 of these
+    # and the second one is empty. Cheers.
+
+    nb_subgoals = len(sub_goals)
+    plural_opt = '' if nb_subgoals == 1 else 's'
+    buff.append(['%d subgoal%s' % (nb_subgoals, plural_opt), ''])
+
+    for idx, sub_goal in enumerate(sub_goals):
+        [ _id, hyps , ccl ] = list(sub_goal)
+        if idx == 0:
+            # we print the environment only for the current subgoal
+            for hyp in list(hyps):
+                lst = map(lambda s: s.encode('utf-8'), hyp.text.split('\n'))
+                buff.append(lst)
+        buff.append('')
+        buff.append('======================== ( %d / %d )' % (idx+1 , nb_subgoals))
+        lines = map(lambda s: s.encode('utf-8'), ccl.text.split('\n'))
+        buff.append(lines)
+        buff.append('')
+
+def show_info():
+    global info_msg
+
+    buff = None
+    for b in vim.buffers:
+        if re.match(".*Infos$", b.name):
+            buff = b
+            break
+
+    del buff[:]
+    if info_msg is not None:
+        lst = info_msg.split('\n')
+        buff.append(lst)
+        info_msg = None
+
+def reset_color():
+    global error_at
+    # Clear current coloring (dirty)
+    if int(vim.eval('b:checked')) != -1:
+        vim.command('call matchdelete(b:checked)')
+        vim.command('let b:checked = -1')
+    if int(vim.eval('b:sent')) != -1:
+        vim.command('call matchdelete(b:sent)')
+        vim.command('let b:sent = -1')
+    if int(vim.eval('b:errors')) != -1:
+        vim.command('call matchdelete(b:errors)')
+        vim.command('let b:errors = -1')
+    # Recolor
+    if encountered_dots:
+        (line, col, _) = encountered_dots[-1]
+        start = { 'line': 0 , 'col': 0 }
+        stop  = { 'line': line + 1, 'col': col }
+        zone = _make_matcher(start, stop)
+        vim.command("let b:checked = matchadd('CheckedByCoq', '%s')" % zone)
+    if len(send_queue) > 0:
+        (l, c, _) = encountered_dots[-1] if encountered_dots else (0,-1,False)
+        r = send_queue.pop()
+        send_queue.append(r)
+        (line, col) = r['stop']
+        start = { 'line': l , 'col': c + 1 }
+        stop  = { 'line': line + 1, 'col': col }
+        zone = _make_matcher(start, stop)
+        vim.command("let b:sent = matchadd('SentToCoq', '%s')" % zone)
+    if error_at:
+        ((sline, scol), (eline, ecol)) = error_at
+        start = { 'line': sline + 1, 'col': scol }
+        stop  = { 'line': eline + 1, 'col': ecol }
+        zone = _make_matcher(start, stop)
+        vim.command("let b:errors = matchadd('CoqError', '%s')" % zone)
+        error_at = None
+
+def rewind_to(line, col):
+    if coqtop is None:
+        print('Internal error: vimbufsync is still being called but coqtop\
+                appears to be down.')
+        print('Please report.')
+        return
+
+    predicate = lambda x: x <= (line, col, True)
+    lst = filter(predicate, encountered_dots)
+    steps = len(encountered_dots) - len(lst)
+    coq_rewind(steps)
+
+#############################
+# Communication with Coqtop #
+#############################
+
+def send_until_fail():
+    """
+    Tries to send every message in [send_queue] to Coq, stops at the first
+    error.
+    When this function returns, [send_queue] is empty.
+    """
+    global encountered_dots, error_at, info_msg
+
+    xml_template = ET.Element('call')
+    xml_template.set('val', 'interp')
+
+    while len(send_queue) > 0:
+        reset_color()
+
+        message_range = send_queue.popleft()
+        message = _between(message_range['start'], message_range['stop'])
+        xml_template.text = message.decode('utf-8')
+
+        send_cmd(xml_template)
+        response = get_answer()
+
+        if response.get('val') == 'good':
+            (eline, ecol) = message_range['stop']
+            if _time_to_collapse(message):
+                idx = rfind(encountered_dots, lambda x: x[2])
+                encountered_dots = encountered_dots[:idx]
+            elt = (eline, ecol + 1, _will_be_collapsed(message))
+            encountered_dots.append(elt)
+
+            optionnal_info = response.find('string')
+            if optionnal_info is not None:
+                info_msg = optionnal_info.text
+        else:
+            send_queue.clear()
+            if response.get('val') == 'fail':
+                info_msg = response.text
+                if response.get('loc_s'):
+                    # TODO: get the [optional] offset in the anwser, which is
+                    # exprimed in char, not counting lines and reduce the "error
+                    # highlight zone"
+                    (line, col) = message_range['start']
+                    (eline, ecol) = message_range['stop']
+                    error_at = ((line, col), (eline, ecol))
+            elif response.get('val') == 'unsafe':
+                print('wtf does "unsafe" mean?')
+            else:
+                print("(ANOMALY) unknown answer: %s" % ET.tostring(response))
+            break
+
+    refresh()
+
+def send_cmd(xml_tree):
+    serialized = ET.tostring(xml_tree)
+    coqtop.stdin.write(serialized)
+
+def get_answer():
+    acc = ''
+    while True:
+        # when read(n) is called, python blocks until n bytes are read. That's
+        # why libraries like "expat" are not used here.
+        # And that's the reason of this code. 
+        tmp = coqtop.stdout.read(1)
+        acc += tmp
+        if tmp == '>':
+            try:
+                elt = ET.fromstring(acc)
+                return elt
+            except ET.ParseError:
+                continue
+
+#################
+# Miscellaneous #
+#################
+
+def _between(begin, end):
+    """
+    Returns a string corresponding to the portion of the buffer between the
+    [begin] and [end] positions.
+    """
+    (bline, bcol) = begin
+    (eline, ecol) = end
+    buf = vim.current.buffer
+    acc = ""
+    for line, str in enumerate(buf[bline:eline + 1]):
+        start = bcol if line == 0 else 0
+        stop  = ecol + 1 if line == eline - bline else len(str)
+        acc += str[start:stop]
+    return acc
+
+def _get_message_range(after):
+    """ See [_find_next_chunk] """
+    (line, col) = after
+    end_pos = _find_next_chunk(line, col)
+    return { 'start':after , 'stop':end_pos } if end_pos is not None else None
+
+def _find_next_chunk(line, col):
+    """
+    Returns the position of the next chunk dot after a certain position.
+    That can either be a bullet if we are in a proof, or "a string" terminated
+    by a dot (outside of a comment, and not denoting a path).
+    """
+    buff = vim.current.buffer
+    if line >= len(buff): return
+    bullets = ['{', '}', '-', '+', '*']
+    # We start by striping all whitespaces (including \n) from the beginning of
+    # the chunk.
+    while buff[line][col:].strip() == '':
+        line += 1
+        col = 0
+    while buff[line][col] == ' ': # FIXME: keeping the stripped line would be
+        col += 1                  #   more efficient.
+
+    # Then we check if the first character of the chunk is a bullet.
+    # Intially I did that only when I was sure to be in a proof (by looking in
+    # [encountered_dots] whether I was after a "collapsable" chunk or not), but
+    #   1/ that didn't play well with coq_to_cursor (as the "collapsable chunk"
+    #      might not have been sent/detected yet).
+    #   2/ The bullet chars can never be used at the *beginning* of a chunk
+    #      outside of a proof. So the check was unecessary.
+    if buff[line][col] in bullets:
+        return (line, col + 1)
+
+    # If the chunk doesn't start with a bullet, we look for a dot.
+    return _find_dot_after(line, col)
+
+def _find_dot_after(line, col):
+    """
+    Returns the position of the next "valid" dot after a certain position.
+    Valid here means: recognized by Coq as terminating an input, so dots in
+    comments or in ident paths are not valid.
+    """
+    b = vim.current.buffer
+    if line >= len(b): return
+    s = b[line][col:]
+    dot_pos = s.find('.')
+    com_pos = s.find('(*')
+    if com_pos == -1 and dot_pos == -1:
+        return _find_dot_after(line + 1, 0)
+    elif dot_pos == -1 or (com_pos > -1 and dot_pos > com_pos):
+        com_end = _skip_comment(line, com_pos + 2 + col, 1)
+        if not com_end: return
+        (line, col) = com_end
+        return _find_dot_after(line, col)
+    elif dot_pos < len(s) - 1 and s[dot_pos + 1] != ' ':
+        # Sometimes dot are used to access module fields, we don't want to stop
+        # just after the module name.
+        # Example: [Require Import Coq.Arith]
+        return _find_dot_after(line, col + dot_pos + 1)
+    elif dot_pos + col > 0 and b[line][col + dot_pos - 1] == '.':
+        # We don't want to capture ".."
+        return _find_dot_after(line, col + dot_pos + 1)
+    else:
+        return (line, dot_pos + col)
+
+def _skip_comment(line, col, nb_left):
+    """
+    Used when we encountered the start of a comment before a valid dot (see
+    [_find_dot_after]).
+    Returns the position of the end of the comment.
+    """
+    if nb_left == 0:
+        return (line, col)
+
+    b = vim.current.buffer
+    if line >= len(b): return
+    s = b[line][col:]
+    com_start = s.find('(*')
+    com_end = s.find('*)')
+    if com_end > -1 and (com_end < com_start or com_start == -1):
+        return _skip_comment(line, col + com_end + 2, nb_left - 1)
+    elif com_start > -1:
+        return _skip_comment(line, col + com_start + 2, nb_left + 1)
+    else:
+        return _skip_comment(line + 1, 0, nb_left)
+
+def _will_be_collapsed(s):
+    """
+    Collapsable part are useful when we want to rewind to a certain position.
+    Indeed when we send something to Coq, a "step" is just a string between two
+    "valid" dots, but when we rewind a step might be /bigger/ than that as Coq
+    doesn't rewind just a "Qed" or "Defined" when it meets one, but the whole
+    proof (returning the "actual" number of steps rewinded).
+    We could just rewind one step at a time until we reach the desired point in
+    the buffer, but this seems more efficient.
+    """
+    if re.match(".*(Theorem|Goal|Lemma|Next Obligation).*", s):
+        return True
+    elif re.match('.*Definition .*', s) and not re.search(':=', s):
+        return True
+    else:
+        return False
+
+def _time_to_collapse(s):
+    """ Used in conjunction with [_will_be_collapsed] """
+    return True if re.match('.*(Qed|Defined)\.$', s) else False
+
+## I thought python was the language with a big stdlib...
+def rfind(lst, cond):
+    tmp = None
+    for idx, elt in enumerate(lst):
+        if cond(elt): tmp = idx
+    return tmp
+
+################################################
+# The ugly through behind regions highlighting #
+################################################
+
+def _make_matcher(start, stop):
+    if start['line'] == stop['line']:
+        return _easy_matcher(start, stop)
+    else:
+        return _hard_matcher(start, stop)
+
+def _easy_matcher(start, stop):
+    startl = ""
+    startc = ""
+    if start['line'] > 0:
+        startl = "\%>{0}l".format(start['line'] - 1)
+    if start['col'] > 0:
+        startc = "\%>{0}c".format(start['col'])
+    return '{0}{1}\%<{2}l\%<{3}c'.format(startl, startc, stop['line'] + 1, stop['col'] + 1)
+
+def _hard_matcher(start, stop):
+    first_start = {'line' : start['line'], 'col' : start['col']}
+    first_stop =  {'line' : start['line'], 'col' : 4242}
+    first_line = _easy_matcher(first_start, first_stop)
+    mid_start = {'line' : start['line']+1, 'col' : 0}
+    mid_stop =  {'line' : stop['line']-1 , 'col' : 4242}
+    middle = _easy_matcher(mid_start, mid_stop)
+    last_start = {'line' : stop['line'], 'col' : 0}
+    last_stop =  {'line' : stop['line'], 'col' : stop['col']}
+    last_line = _easy_matcher(last_start, last_stop)
+    return "{0}\|{1}\|{2}".format(first_line, middle, last_line)
---
+++ autoload/coquille.vim
@@ -0,0 +1,81 @@
+let s:current_dir=expand("<sfile>:p:h") 
+" Load vimbufsync if not already done
+call vimbufsync#init()
+
+py import sys, vim
+py if not vim.eval("s:current_dir") in sys.path:
+\    sys.path.append(vim.eval("s:current_dir")) 
+py import coquille
+
+function! coquille#ShowPanels()
+    " open the Goals & Infos panels before going back to the main window
+    let l:winnb = winnr()
+    rightbelow vnew Goals
+        setlocal buftype=nofile
+        setlocal noswapfile
+        let s:goal_buf = bufnr("%")
+    rightbelow new Infos
+        setlocal buftype=nofile
+        setlocal noswapfile
+        let s:info_buf = bufnr("%")
+    execute l:winnb . 'winc w'
+endfunction
+
+function! coquille#KillSession()
+    execute 'bdelete' . s:goal_buf
+    execute 'bdelete' . s:info_buf
+    py coquille.kill_coqtop()
+
+    setlocal ei=InsertEnter
+endfunction
+
+function! coquille#FNMapping()
+    "" --- Function keys bindings
+    "" Works under all tested config.
+    map <silent> <F2> :CoqUndo<CR>
+    map <silent> <F3> :CoqNext<CR>
+    map <silent> <F4> :CoqToCursor<CR>
+endfunction
+
+function! coquille#CoqideMapping()
+    "" ---  CoqIde key bindings
+    "" Unreliable: doesn't work with all terminals, doesn't work through tmux,
+    ""  etc.
+    map <silent> <C-A-Up>    :CoqUndo<CR>
+    map <silent> <C-A-Down>  :CoqNext<CR>
+    map <silent> <C-A-Right> :CoqToCursor<CR>
+endfunction
+
+function! coquille#Launch()
+    " initialize the plugin (launch coqtop)
+    py coquille.launch_coq()
+
+    call coquille#ShowPanels()
+
+    " make the different commands accessible
+    command! CoqNext py coquille.coq_next()
+    command! CoqUndo py coquille.coq_rewind()
+    command! CoqToCursor py coquille.coq_to_cursor()
+    command! CoqKill call coquille#KillSession()
+
+    " Automatically sync the buffer when entering insert mode: this is usefull
+    " when we edit the portion of the buffer which has already been sent to coq,
+    " we can then rewind to the appropriate point.
+    " It's still incomplete though, the plugin won't sync when you undo or
+    " delete some part of your buffer. So the highlighting will be wrong, but
+    " nothing really problematic will happen, as sync will be called the next
+    " time you explicitly call a command (be it 'rewind' or 'interp')
+    au InsertEnter <buffer> py coquille.sync()
+endfunction
+
+function! coquille#Register()
+    hi CheckedByCoq ctermbg=17 guibg=LightGreen
+    hi SentToCoq ctermbg=60 guibg=60
+    hi link CoqError Error
+
+    let b:checked = -1
+    let b:sent    = -1
+    let b:errors  = -1
+
+    command! -bar -buffer -nargs=0 CoqLaunch call coquille#Launch()
+endfunction
